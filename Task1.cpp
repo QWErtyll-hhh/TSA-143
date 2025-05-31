10e47eab8861ce05fc835840399a2a1d893d765b
Milyaev Gregory TCA-143 <achokdur521@gmail.com>
2025-05-31 10:25:06 +0300

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -1,30 +1,43 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
 /**
- *@brief - Функция для вычисления С по заданному уравнению 
  *@param x - значение переменной x
  *@param y - значение переменной y
  *@param z - значение переменной z
- *@return - возвращает рассчитанное значение
+ *@return - возращает вычесленное значение
  */
-double getb(const double x, const double y, const double z);
+double geta(const double x, const double y, const double z );
+
 /**
- *@brief - Точка входа в программу 
- *@return - возвращает 0, если программа выполнена корректно
+ *@param x - значение переменной x
+ *@param y - значение переменной y
+ *@param z - значение переменной z
+ *@return - возращает вычесленное значение
+ */
+
+ double getb(const double x, const double y, const double z);
+/**
+ *@return - возращает 0 если программа выполнена корректно
  */
 int main()
 
 {
   const double x = 0.78;
-  const double y = 1.24;
+  const double y = 1.24;    
   const double z = 0.5;
-    cout << "a= "<<geta(x,y,z)<<endl;
-    cout << "b= "<<getb(x,y,z);
+    cout<<"a = "<<geta(x,y,z)<<endl;
+    cout<<"b = "<<getb(x,y,z);
     return 0;
 }
-double geta(const double x, const double y, const double z)
+
+double geta(const double x, const double y, const double z )
 {
-  return 2 * pow(z, x)/sqrt(pow(y, x)*pow(cos(x + y), 2))-3*z;
+    return 2 * pow(z, x)/sqrt(pow(y, x)*pow(cos(x + y), 2))-3*z;
 }
+
 double getb(const double x, const double y, const double z)
 {
-  return  x * exp(sqrt(z)) * cos(pow(x, 2) / y * z);
+    return x * exp(sqrt(z)) * cos(pow(x, 2) / y * z);
 }
